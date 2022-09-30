36bcaac9e5ef8de5adefd344dd2759f857a392a4
rease <cftcview@gmail.com>
2022-09-30 06:16:18 +0500

    fix random

--- src/extra/random.ts
+++ src/extra/random.ts
@@ -2,6 +2,6 @@ import random from '../base/random'
 
 const __random__ = (): number => random() || __random__()
 const randomBetween = (
-  min = 0, max = 0,
+  min = 0, max = min + 1,
 ): number => __random__() * (max - min) + min
 export default randomBetween
