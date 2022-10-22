d24e05b0243f90cb9eaa7606a7acb5c43edfde18
rease <cftcview@gmail.com>
2022-10-22 15:08:34 +0500

    change __test__

--- __test__/index.js
+++ __test__/index.js
@@ -1,3 +1,9 @@
+const MathSafe = require('../ecma')
+const MathAll = require('..')
+
+console.log(MathSafe)
+console.log(MathAll)
+
 const scale = require('..').scale
 
 console.log(scale(40, 1, 100, 0.1, 10))
